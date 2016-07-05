30679287f2a98f06b156938d74b23833a9a16f52
unknown <>
2016-07-05 15:03:50 +0800

    suck my ass

---
+++ HW1_1/Prob1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main() {
+	int sum = 0;
+	int i = 0;
+
+	cout << "Enter a sequence of numbers to be summed: ";
+
+	while (cin >> i && i >= 0) {
+		sum += i;
+	}
+
+	cout << "Sum is: " << sum << endl;
+	system("pause");
+	return 0;
+}
